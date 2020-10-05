dfde22037863ba908dc1278c81be473842aed2c8
Olivier JEAN-BAPTISTE <ojeanbaptiste75@gmail.com>
2020-10-05 15:13:44 +0200

    finished metaboxes

--- wp-content/themes/letsdoit/functions.php
+++ wp-content/themes/letsdoit/functions.php
@@ -16,6 +16,7 @@ function letsdoit_register_assets(){
     wp_deregister_script('jquery'); // Disable Bootstrap's default jQuery
     wp_register_script('jquery', 'https://code.jquery.com/jquery-3.5.1.slim.min.js', [], false, true); // Enable our Bootstrap
     wp_enqueue_style('bootstrap'); // https://developer.wordpress.org/reference/functions/wp_enqueue_style/
+    wp_enqueue_script('bootstrap');
 }
 
 function letsdoit_title_separator(){
@@ -60,8 +61,6 @@ add_action('wp_enqueue_scripts', 'letsdoit_register_assets'); // https://develop
 add_filter('document_title_separator', 'letsdoit_title_separator'); // https://developer.wordpress.org/reference/functions/wp_get_document_title/
 add_filter('nav_menu_css_class', 'letsdoit_menu_class');
 add_filter('nav_menu_link_attributes', 'letsdoit_menu_link_class');
-add_action('add_meta_boxes', 'letsdoit_add_custom_box'); // https://developer.wordpress.org/reference/hooks/add_meta_boxes/
-add_action('save_post', 'letsdoit_save_sponso');
 
 require_once('metaboxes/sponso.php');
 
--- wp-content/themes/letsdoit/metaboxes/sponso.php
+++ wp-content/themes/letsdoit/metaboxes/sponso.php
@@ -3,18 +3,22 @@
 class SponsoMetaBox{
 
     const META_KEY = 'letsdoit_sponso';
+    const NONCE = '_letsdoit_sponso_nonce';
 
     public static function register(){
-        add_action('add_meta_boxes', [self::class, 'add']);
+        add_action('add_meta_boxes', [self::class, 'add'], 10, 2);
         add_action('save_post', [self::class, 'save']);
     }
 
-    public static function add (){
-        add_meta_box(self::META_KEY, 'Sponsoring', [self::class, 'render'], 'post', 'side'); 
+    public static function add ($postType, $post){
+        if ($postType === 'post' && current_user_can( 'publish_posts', $post)){
+            add_meta_box(self::META_KEY, 'Sponsoring', [self::class, 'render'], 'post', 'side');
+        }  
     }
 
     public static function render($post){
         $value = get_post_meta($post->ID, self::META_KEY, true);
+        wp_nonce_field(self::NONCE, self::NONCE);
         ?>
         <input type="hidden" value="0" name="<?= self::META_KEY ?>">
         <input type="checkbox" value="1" name="<?= self::META_KEY ?>" <?php checked($value, '1') ?> >
@@ -23,7 +27,11 @@ class SponsoMetaBox{
     }
 
     public static function save($post){
-        if (array_key_exists(self::META_KEY, $_POST) && current_user_can( 'edit_post', $post)){ // https://developer.wordpress.org/reference/functions/current_user_can/
+        if (
+            array_key_exists(self::META_KEY, $_POST) && 
+            current_user_can( 'publish_posts', $post) && // https://developer.wordpress.org/reference/functions/current_user_can/
+            wp_verify_nonce($_POST[self::NONCE], self::NONCE) // https://wordpress.org/support/article/roles-and-capabilities/
+            ){
             if ($_POST[self::META_KEY] === '0'){
                 delete_post_meta($post, self::META_KEY); // https://developer.wordpress.org/reference/functions/delete_post_meta/
             }else{
